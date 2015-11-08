bc53f3dadcd7709491901d55ae534b4f8589ec19
Jerry Butiong <jerry.butiong@hotmail.com>
2015-11-08 15:21:48 +0800

    Added EventAndDelegate3 sample

---
+++ DemoCode/EventAndDelegate3/MailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object sender, VideoEventArgs args)
+        {
+            Console.WriteLine("Mail Service: Sending an email....." + args.Video.Title);
+        }
+    }
+}
